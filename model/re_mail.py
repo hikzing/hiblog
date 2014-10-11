3efb3e96d48e6f55386e8569a0da399f4b06713c
linzhaoyu <kzinglzy@gmail.com>
2014-10-11 10:51:02 +0800

    change dir structure

--- model/re_mail.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding:utf-8
-import re
-
-RE_MAIL = re.compile(r'^\w+[-+.\w]*@\w+([-.]\w+)*\.\w+([-.]\w+)*$')
-
-if __name__ == '__main__':
-    mail = 'kzing@gmail.com'
-    print(RE_MAIL.match(mail))
