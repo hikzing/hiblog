acbef15c54d44826867279b381ce70342093ba3c
linzhaoyu <kzinglzy@gmail.com>
2014-11-04 08:19:31 +0800

    update site titte implement

--- hiblog/_base/setting.py
+++ hiblog/_base/setting.py
@@ -1,10 +1,12 @@
 # coding: utf-8
 
-
 # Home
 HOME_TITLE = 'Hi, Blog'                   # 主标题
 HOME_DESC = 'A Clean Blog System'         # 副标题
 HOME_PAGE_LIMIT = 8                       # 首页每页显示的文章数目
+
+# SITE
+SITE_TITLE = "Hi, Blog"                   # 网页显示的标题
 
 
 # About
@@ -15,13 +17,13 @@ ABOUT_CONTENT = 'I am who I am'           # 具体内容
 # Contact
 CONTACT_TITLE = 'Contact Me'              # 主标题
 CONTACT_DESC = "Have questions? I have answers (maybe)."  # 副标题
-CONTACT_CONTENT = "Just Contact Me"           # 具体内容
+CONTACT_CONTENT = "Just Contact Me"       # 具体内容
 
 # Admin
 ADMIN_PAGE_LIMIT = 5                      # 管理页面的预览文章时的每页显示数目
 
 # Blog评论
-DISQUS_SHORTNAME = 'kzing'     # 你在 disqus 上的名字
+DISQUS_SHORTNAME = 'kzing'                # 你在 disqus 上的名字
 
 # Copyright
-COPYRIGHT = "SOME_NAME"              # 版权所属者.
+COPYRIGHT = "SOME_NAME"                   # 网页底部版权标识
